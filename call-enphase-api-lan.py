2c160030a89a7f0ee98af6614dc40c4189b1c2e2
Scalextrix <steven.campbell100@gmail.com>
2017-01-15 11:51:34 +0000

    Update call-enphase-api-lan.py

--- call-enphase-api-lan.py
+++ call-enphase-api-lan.py
@@ -23,12 +23,14 @@ def lan_wan_choose():
         	lan_api()
 	elif lan_wan == "n" or lan_wan == "no" or lan_wan == "web":
 		web_api()
+	elif lan_wan == "exit"
+		sys.exit("Exiting")
 	else:
-		sys.exit("Sorry: You must choose 'Y' or 'N'")
+		print("Sorry: You must choose 'y', 'yes', 'LAN' or 'n', 'no', 'WEB' or 'exit' to Exit")
 
 def lan_api():
-	if os.path.isfile("APIlan.db"):
-		print("Found Enphase API LAN database")
+	if os.path.isfile("API.db"):
+		print("Found Enphase API database")
 	else:
 		envoy_ip = raw_input ("What is your Enphase Envoy IP address: ")
 		solarcoin_address = raw_input ("What is your SolarCoin Address: ")
@@ -39,16 +41,18 @@ def lan_api():
 		longitude = raw_input ("What is the Longitude of your installation: ")
 		message = raw_input ("Add an optional message describing your system: ")
 		rpi = raw_input ("If you are staking on a Raspberry Pi note the Model: ")
-		conn = sqlite3.connect("APIlan.db")
+		conn = sqlite3.connect("API.db")
 		c = conn.cursor()
-		c.execute('''CREATE TABLE IF NOT EXISTS SYSTEMDETAILS (envoyip TEXT, SLRaddress TEXT, panelid TEXT, inverterid TEXT, pkwatt TEXT, lat TEXT, lon TEXT, msg TEXT, pi TEXT)''')
-		c.execute("INSERT INTO SYSTEMDETAILS VALUES (?,?,?,?,?,?,?,?,?);", (envoy_ip, solarcoin_address, solar_panel, solar_inverter, peak_watt, latitude, longitude, message, rpi,))
+		c.execute('''CREATE TABLE IF NOT EXISTS LANDETAILS (envoyip, TEXT)''')
+		c.execute("INSERT INTO LANDETAILS VALUES (?);", (envoy_ip,))
+		c.execute('''CREATE TABLE IF NOT EXISTS SYSTEMDETAILS (SLRaddress TEXT, panelid TEXT, inverterid TEXT, pkwatt TEXT, lat TEXT, lon TEXT, msg TEXT, pi TEXT)''')
+		c.execute("INSERT INTO SYSTEMDETAILS VALUES (?,?,?,?,?,?,?,?,?);", (solarcoin_address, solar_panel, solar_inverter, peak_watt, latitude, longitude, message, rpi,))
 		conn.commit()		
 		conn.close()
 
-	conn = sqlite3.connect("APIlan.db")
+	conn = sqlite3.connect("API.db")
 	c = conn.cursor()
-	envoy_ip = c.execute('select envoyip from SYSTEMDETAILS').fetchall()
+	envoy_ip = c.execute('select envoyip from LANDETAILS').fetchall()
 	solarcoin_address = c.execute('select SLRaddress from SYSTEMDETAILS').fetchall()
 	solar_panel = c.execute('select panelid from SYSTEMDETAILS').fetchall()
 	solar_inverter = c.execute('select inverterid from SYSTEMDETAILS').fetchall()
@@ -85,8 +89,8 @@ def lan_api():
 def wan_api():
 	api_key = ("6ba121cb00bcdafe7035d57fe623cf1c&usf1c&usf1c")
 
-	if os.path.isfile("APIweb.db"):
-		print("Found Enphase API web database")
+	if os.path.isfile("API.db"):
+		print("Found Enphase API database")
 	else:
 		system_id = raw_input ("What is your Enphase System ID: ")
 		user_id = raw_input ("What is your Enphase User ID: ")
@@ -98,17 +102,19 @@ def wan_api():
 		longitude = raw_input ("What is the Longitude of your installation: ")
 		message = raw_input ("Add an optional message describing your system: ")
 		rpi = raw_input ("If you are staking on a Raspberry Pi note the Model: ")
-		conn = sqlite3.connect("APIweb.db")
+		conn = sqlite3.connect("API.db")
 		c = conn.cursor()
-		c.execute('''CREATE TABLE IF NOT EXISTS SYSTEMDETAILS (systemid TEXT, userid TEXT, SLRaddress TEXT, panelid TEXT, inverterid TEXT, pkwatt TEXT, lat TEXT, lon TEXT, msg TEXT, pi TEXT)''')
-		c.execute("INSERT INTO SYSTEMDETAILS VALUES (?,?,?,?,?,?,?,?,?,?);", (system_id, user_id, solarcoin_address, solar_panel, solar_inverter, peak_watt, latitude, longitude, message, rpi,))
+		c.execute('''CREATE TABLE IF NOT EXISTS WEBDETAILS (systemid TEXT, userid TEXT)''')
+		c.execute("INSERT INTO WEBDETAILS VALUES (?,?);", (system_id, user_id,))
+		c.execute('''CREATE TABLE IF NOT EXISTS SYSTEMDETAILS (SLRaddress TEXT, panelid TEXT, inverterid TEXT, pkwatt TEXT, lat TEXT, lon TEXT, msg TEXT, pi TEXT)''')
+		c.execute("INSERT INTO SYSTEMDETAILS VALUES (?,?,?,?,?,?,?,?,?,?);", (solarcoin_address, solar_panel, solar_inverter, peak_watt, latitude, longitude, message, rpi,))
 		conn.commit()		
 		conn.close()
 
-	conn = sqlite3.connect("APIweb.db")
+	conn = sqlite3.connect("API.db")
 	c = conn.cursor()
-	system_id = c.execute('select systemid from SYSTEMDETAILS').fetchall()
-	user_id = c.execute('select userid from SYSTEMDETAILS').fetchall()
+	system_id = c.execute('select systemid from WEBDETAILS').fetchall()
+	user_id = c.execute('select userid from WEBDETAILS').fetchall()
 	solarcoin_address = c.execute('select SLRaddress from SYSTEMDETAILS').fetchall()
 	solar_panel = c.execute('select panelid from SYSTEMDETAILS').fetchall()
 	solar_inverter = c.execute('select inverterid from SYSTEMDETAILS').fetchall()
@@ -143,6 +149,8 @@ def wan_api():
 	energy_today = float(energy_today)
 	total_energy = (energy_lifetime + energy_today) / 1000000
 	print("Total Energy MWh: {:.6f}") .format(total_energy)
+
+lan_wan_choose()
 
 print("Initiating SolarCoin")
 energylifetime = str('Note this is all public information '+solar_panel+'; '+solar_inverter+'; '+peak_watt+'kW ;'+latitude+','+longitude+'; '+message+'; '+rpi+'; Total MWh: {}' .format(total_energy)+'; Powered by Enphase Energy: http://enphase.com')
