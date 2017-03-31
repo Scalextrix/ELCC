f1ca41ef0e0540518cef394a9ec5d64e85a50369
Scalextrix <solarsteve100@gmail.com>
2017-03-31 13:37:14 +0100

    collapsed all repetitive code

--- Enphase/datalogger.py
+++ Enphase/datalogger.py
@@ -6,7 +6,7 @@ instructs the solarcoin daemon to make a transaction to record onto blockchain""
 __author__ = "Steven Campbell AKA Scalextrix"
 __copyright__ = "Copyright 2017, Steven Campbell"
 __license__ = "The Unlicense"
-__version__ = "2.3"
+__version__ = "3.0"
 
 import gc
 import getpass
@@ -29,14 +29,21 @@ def calculateamounttosend():
         print ('Based on wallet balance of {} amount to send to self set to {} SLR') .format(wallet_balance, send_amount)
         return send_amount
 
-def checkfordatabases():
-	if os.path.isfile("APIlan.db"):
-		lan_wan = "y"	
-	elif os.path.isfile("APIweb.db"):
-		lan_wan = "n"
-	else:
-		lan_wan = raw_input("Is the Inverter on your LAN: ").lower()
-	return lan_wan
+def databasecreate():
+        conn = sqlite3.connect(dbname)
+        c = conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS SYSTEMDETAILS (systemid TEXT, userid TEXT, envoyip TEXT, SLRaddress TEXT, panelid TEXT, inverterid TEXT, pkwatt TEXT, lat TEXT, lon TEXT, msg TEXT, pi TEXT)''')
+        c.execute("INSERT INTO SYSTEMDETAILS VALUES (?,?,?,?,?,?,?,?,?,?,?);", (system_id, user_id, envoy_ip, solarcoin_address, solar_panel, solar_inverter, peak_watt, latitude, longitude, message, rpi,))
+        conn.commit()
+        conn.close()
+	
+def databasenamebroken():
+        del solarcoin_passphrase
+        gc.collect()
+        print "Exiting in 10 seconds: Database name corrupted, delete *.db and try again"
+        time.sleep(10)
+        sys.exit()
+
 
 def inverterqueryincrement():
         """ Sets the frequency that the solar inverter is queried, value in Seconds; max 300 seconds set to stay within
@@ -46,6 +53,7 @@ def inverterqueryincrement():
                 inverter_query_increment = int(86400 / system_watt)
         else:
                 inverter_query_increment = 300
+	# inverter_query_increment = 86 # Uncomment for testing
         return inverter_query_increment
 
 def maintainenergylog():
@@ -88,6 +96,11 @@ def refreshenergylogandsleep():
 	time.sleep(inverter_query_increment)
 
 def retrievecommoncredentials():
+	conn = sqlite3.connect(dbname)
+        c = conn.cursor()
+        system_id = str(c.execute('select systemid from SYSTEMDETAILS').fetchone()[0])
+        user_id = str(c.execute('select userid from SYSTEMDETAILS').fetchone()[0])
+        envoy_ip = str(c.execute('select envoyip from SYSTEMDETAILS').fetchone()[0])
         solarcoin_address = str(c.execute('select SLRaddress from SYSTEMDETAILS').fetchone()[0])
         solar_panel = str(c.execute('select panelid from SYSTEMDETAILS').fetchone()[0])
         solar_inverter = str(c.execute('select inverterid from SYSTEMDETAILS').fetchone()[0])
@@ -97,12 +110,12 @@ def retrievecommoncredentials():
         message = str(c.execute('select msg from SYSTEMDETAILS').fetchone()[0])
         rpi = str(c.execute('select pi from SYSTEMDETAILS').fetchone()[0])
         conn.close()
-        return {'solarcoin_address':solarcoin_address, 'solar_panel':solar_panel, 'solar_inverter':solar_inverter, 'peak_watt':peak_watt, 'latitude':latitude, 'longitude':longitude, 'message':message, 'rpi':rpi}
+        return {'system_id':system_id, 'user_id':user_id, 'envoy_ip':envoy_ip, 'solarcoin_address':solarcoin_address, 'solar_panel':solar_panel, 'solar_inverter':solar_inverter, 'peak_watt':peak_watt, 'latitude':latitude, 'longitude':longitude, 'message':message, 'rpi':rpi}
 
 def sleeptimer():
 	energy_left = (energy_reporting_increment - (energy_log['end_energy'] - energy_log['start_energy'])) * 1000
 	print ("Waiting for another {:.3f} kWh to be generated, will check again in {:.0f} seconds (approx {:.2f} days)") .format(energy_left, inverter_query_increment, (inverter_query_increment/86400))
-	print "*************************************************"
+	print ("******** "+manufacturer_attribution+" ********")
 	time.sleep(inverter_query_increment)
 
 def timestamp():
@@ -133,117 +146,75 @@ def writetoblockchain():
 
 # Sets the frequency with which the reports will be made to block-chain, value in MWh e.g. 0.01 = 10kWh
 energy_reporting_increment = 0.01
-
 manufacturer_attribution = "Powered by Enphase Energy: https://enphase.com"
-api_key = ("6ba121cb00bcdafe7035d57fe623cf1c&usf1c&usf1c")
+api_key = "6ba121cb00bcdafe7035d57fe623cf1c&usf1c&usf1c"
 
 solarcoin_passphrase = passphrasetest()	
-lan_wan = checkfordatabases()
 
-if lan_wan == "y" or lan_wan == "yes" or lan_wan == "lan":
-	dbname="APIlan.db"
-	if os.path.isfile(dbname):
-		print("Found Enphase API LAN database")
-	else:
-		envoy_ip = raw_input ("What is your Enphase Envoy IP address: ")
-		solarcoin_address = raw_input ("What is your SolarCoin Address: ")
-		solar_panel = raw_input ("What is the Make, Model & Part Number of your solar panel: ")
-		solar_inverter = raw_input ("What is the Make, Model & Part Number of your inverter: ")
-		while True:
-			peak_watt = raw_input ("In kW (kilo-Watts), what is the peak output of your system: ")
-			try:
-				peak_watt = float(peak_watt)
-				break
-			except ValueError:
-				print "Error: You must enter numbers and decimal point only e.g. 3.975"
-		latitude = raw_input ("What is the Latitude of your installation: ")
-		longitude = raw_input ("What is the Longitude of your installation: ")
-		message = raw_input ("Add an optional message describing your system: ")
-		rpi = raw_input ("If you are staking on a Raspberry Pi note the Model: ")
-		conn = sqlite3.connect(dbname)
-		c = conn.cursor()
-		c.execute('''CREATE TABLE IF NOT EXISTS SYSTEMDETAILS (envoyip TEXT, SLRaddress TEXT, panelid TEXT, inverterid TEXT, pkwatt TEXT, lat TEXT, lon TEXT, msg TEXT, pi TEXT)''')
-		c.execute("INSERT INTO SYSTEMDETAILS VALUES (?,?,?,?,?,?,?,?,?);", (envoy_ip, solarcoin_address, solar_panel, solar_inverter, peak_watt, latitude, longitude, message, rpi,))
-		conn.commit()		
-		conn.close()
-
-	conn = sqlite3.connect(dbname)
-	c = conn.cursor()
-	envoy_ip = str(c.execute('select envoyip from SYSTEMDETAILS').fetchone()[0])
-	comm_creds = retrievecommoncredentials()
-	
-        inverter_query_increment = float(inverterqueryincrement())
-	url = ("http://"+envoy_ip+"/api/v1/production")
-	
-	while True:
-		timestamp()
-                json_data = urltestandjsonload()
-		total_energy = float(json_data['wattHoursLifetime'])/1000000
-		print("Total Energy MWh: {:.6f}") .format(total_energy)
-		energy_log = maintainenergylog()
-		if energy_log['end_energy'] >= (energy_log['start_energy'] + energy_reporting_increment):
-                        send_amount = calculateamounttosend()
-			writetoblockchain()
-			print manufacturer_attribution
-			refreshenergylogandsleep()
-		else:
-			sleeptimer()	
-	
-elif lan_wan == "n" or lan_wan == "no" or lan_wan == "web":
-	dbname="APIweb.db"
-	if os.path.isfile(dbname):
-		print("Found Enphase API web database")
-	else:
-		system_id = raw_input ("What is your Enphase System ID: ")
-		user_id = raw_input ("What is your Enphase User ID: ")
-		solarcoin_address = raw_input ("What is your SolarCoin Address: ")
-		solar_panel = raw_input ("What is the Make, Model & Part Number of your solar panel: ")
-		solar_inverter = raw_input ("What is the Make, Model & Part Number of your inverter: ")
-		while True:
-			peak_watt = raw_input ("In kW (kilo-Watts), what is the peak output of your system: ")
-			try:
-				peak_watt = float(peak_watt)
-				break
-			except ValueError:
-				print "Error: You must enter numbers and decimal point only e.g. 3.975"
-		latitude = raw_input ("What is the Latitude of your installation: ")
-		longitude = raw_input ("What is the Longitude of your installation: ")
-		message = raw_input ("Add an optional message describing your system: ")
-		rpi = raw_input ("If you are staking on a Raspberry Pi note the Model: ")
-		conn = sqlite3.connect(dbname)
-		c = conn.cursor()
-		c.execute('''CREATE TABLE IF NOT EXISTS SYSTEMDETAILS (systemid TEXT, userid TEXT, SLRaddress TEXT, panelid TEXT, inverterid TEXT, pkwatt TEXT, lat TEXT, lon TEXT, msg TEXT, pi TEXT)''')
-		c.execute("INSERT INTO SYSTEMDETAILS VALUES (?,?,?,?,?,?,?,?,?,?);", (system_id, user_id, solarcoin_address, solar_panel, solar_inverter, peak_watt, latitude, longitude, message, rpi,))
-		conn.commit()		
-		conn.close()
-
-	conn = sqlite3.connect(dbname)
-	c = conn.cursor()
-	system_id = str(c.execute('select systemid from SYSTEMDETAILS').fetchone()[0])
-	user_id = str(c.execute('select userid from SYSTEMDETAILS').fetchone()[0])
-	comm_creds = retrievecommoncredentials()
-
-        inverter_query_increment = float(inverterqueryincrement())
-	url = ("https://api.enphaseenergy.com/api/v2/systems/"+system_id+"/summary?&key="+api_key+"&user_id="+user_id)
-	while True:
-		timestamp()
-                json_data = urltestandjsonload()
-		energy_lifetime = float(json_data['energy_lifetime'])
-		energy_today = float(json_data['energy_today'])
-		total_energy = (energy_lifetime + energy_today) / 1000000
-		print("Total Energy MWh: {:.6f}") .format(total_energy)
-		energy_log = maintainenergylog()
-		if energy_log['end_energy'] >= (energy_log['start_energy'] + energy_reporting_increment):
-                        send_amount = calculateamounttosend()
-			writetoblockchain()
-			print manufacturer_attribution
-			refreshenergylogandsleep()		
-		else:
-			sleeptimer()
-
+if os.path.isfile("APIlan.db"):
+        print "Found API LAN database"
+        dbname = "APIlan.db"
+elif os.path.isfile("APIweb.db"):
+        print "Found API web database"
+        dbname = "APIweb.db"
 else:
-	del solarcoin_passphrase
-	gc.collect()
-	print "Exiting in 10 seconds: You must choose 'y' or 'n'"
-        time.sleep(10)
-	sys.exit()
+        print "No database found, please complete the following credentials: "
+        solarcoin_address = raw_input ("What is your SolarCoin Address: ")
+        solar_panel = raw_input ("What is the Make, Model & Part Number of your solar panel: ")
+        solar_inverter = raw_input ("What is the Make, Model & Part Number of your inverter: ")
+        while True:
+                peak_watt = raw_input ("In kW (kilo-Watts), what is the peak output of your system: ")
+                try:
+                        peak_watt = float(peak_watt)
+                        break
+                except ValueError:
+                        print "Error: You must enter numbers and decimal point only e.g. 3.975"
+        latitude = raw_input ("What is the Latitude of your installation: ")
+        longitude = raw_input ("What is the Longitude of your installation: ")
+        message = raw_input ("Add an optional message describing your system: ")
+        rpi = raw_input ("If you are staking on a Raspberry Pi note the Model: ")
+        lan_web = raw_input ("Is the Inverter on your LAN: ")
+        if lan_web == "y" or lan_web == "yes" or lan_web == "lan":
+                dbname="APIlan.db"
+                system_id = ""
+                user_id = ""
+                envoy_ip = raw_input ("What is the IP address of your Inverter: ")
+                databasecreate()
+        elif lan_web == "n" or lan_web == "no" or lan_web == "web":
+                dbname="APIweb.db"
+                system_id = raw_input ("What is your Enphase System ID: ")
+                user_id = raw_input ("What is your Enphase User ID: ")
+                envoy_ip = ""
+                databasecreate()
+        else:
+                del solarcoin_passphrase
+                gc.collect()
+                print "Exiting in 10 seconds: You must choose 'y' or 'n'"
+                time.sleep(10)
+                sys.exit()
+
+comm_creds = retrievecommoncredentials()
+inverter_query_increment = float(inverterqueryincrement())
+
+while True:
+        timestamp()
+        if os.path.isfile("APIlan.db"):
+                url = ("http://"+comm_creds['envoy_ip']+"/api/v1/production")
+                json_data = urltestandjsonload()
+                total_energy = float(json_data['wattHoursLifetime'])/1000000
+        elif os.path.isfile("APIweb.db"):
+                url = ("https://api.enphaseenergy.com/api/v2/systems/"+comm_creds['system_id']+"/summary?&key="+api_key+"&user_id="+comm_creds['user_id'])
+                json_data = urltestandjsonload()
+                total_energy = (float(json_data['energy_lifetime']) + float(json_data['energy_today'])) / 1000000
+        else:
+                databasenamebroken()
+
+        print("Total Energy MWh: {:.6f}") .format(total_energy)
+        energy_log = maintainenergylog()
+
+        if energy_log['end_energy'] >= (energy_log['start_energy'] + energy_reporting_increment):
+                send_amount = calculateamounttosend()
+                writetoblockchain()
+                refreshenergylogandsleep()
+        else:
+                sleeptimer()
