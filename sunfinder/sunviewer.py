8d0544c974a09f1eff4d5dedabbeb23322606f0f
Scalextrix <solarsteve100@gmail.com>
2017-09-15 18:46:19 +0100

    chart layout & formatting

--- sunfinder/sunviewer.py
+++ sunfinder/sunviewer.py
@@ -39,38 +39,44 @@ energyplot = [float(f[0]) for f in energyplot]
 incenergyplot = [float(f[0]) for f in incenergyplot]
 blocknumber = [int(f[0]) for f in blocknumber]
 
+
+plt.subplots(figsize=(16, 12))
+plt.subplots_adjust(left=0.06, bottom=0.13, right=0.98, top=0.9, wspace=.2, hspace=.6)
 if xaxischooser == 'date':
-	plt.title('Data from ElectriCChain')
 	dates = [a[0][a[0].rfind(';')+1:] for a in datetime]
 	date = [dt.datetime.strptime(d,'%Y-%m-%d %H:%M:%S') for d in dates]
+	plt.figure(1)
+	plt.subplot(211)
+	plt.title('TOTAL Energy data from ElectriCChain')
 	plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
 	plt.gca().xaxis.set_major_locator(mdates.AutoDateLocator())
-	plt.figure(1)
-	plt.subplot(211)
 	plt.plot(date, energyplot, 'go')
-	plt.ylabel('TOTAL Energy MWh')
+	plt.ylabel('Energy MWh')
 	plt.xlabel('Date & Time (UTC)')
-	plt.xticks(rotation='vertical')
-	plt.subplots_adjust(bottom=.4)
+	plt.xticks(rotation=45)
 	plt.subplot(212)
+	plt.title('INCREMENTAL Energy data from ElectriCChain')
+	plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
+	plt.gca().xaxis.set_major_locator(mdates.AutoDateLocator())
 	plt.plot(date, incenergyplot, 'ro')
-	plt.ylabel('INCREMENTAL Energy MWh')
-	plt.xticks(rotation='vertical')
-	plt.subplots_adjust(bottom=.4)
+	plt.ylabel('Energy MWh')
+	plt.xlabel('Date & Time (UTC)')
+	plt.xticks(rotation=45)
 	plt.show()
 elif xaxischooser == 'blocks':
-	plt.title('Data from ElectriCChain')
 	plt.figure(1)
 	plt.subplot(211)
+	plt.title('TOTAL Energy data from ElectriCChain')
 	plt.plot(blocknumber, energyplot, 'go')
-	plt.ylabel('TOTAL Energy MWh')
+	plt.ylabel('Energy MWh')
 	plt.xlabel('SolarCoin Blocks')
-	plt.subplots_adjust(bottom=.4)
+	plt.xticks(rotation=45)
 	plt.subplot(212)
+	plt.title('INCREMENTAL Energy data from ElectriCChain')
 	plt.bar(blocknumber, incenergyplot, width=10.0, align='center', color='r')
-	plt.ylabel('INCREMENTAL Energy MWh')
+	plt.ylabel('Energy MWh')
 	plt.xlabel('SolarCoin Blocks')
-	plt.subplots_adjust(bottom=.4)
+	plt.xticks(rotation=45)
 	plt.show()
 else:
 	print 'You must either choose "date" or "blocks", exit in 10 seconds'
