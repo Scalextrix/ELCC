9fed2607d2cab06ea9b134cef3190a8b967f882f
Scalextrix <solarsteve100@gmail.com>
2017-09-16 13:06:11 +0100

    Added atlas with lat/lon (only charts positive floats)

--- sunfinder/sunviewer.py
+++ sunfinder/sunviewer.py
@@ -10,6 +10,8 @@ __version__ = "1.0"
 
 
 import datetime as dt
+import matplotlib as mpl
+from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
@@ -27,25 +29,29 @@ if userselector == '':
 	incenergyplot = c.execute('select incrementmwh FROM SOLARDETAILS').fetchall()
 	blocknumber = c.execute('select block FROM SOLARDETAILS').fetchall()
 	datetime = c.execute('select period FROM SOLARDETAILS').fetchall()
+	longitudes = c.execute('select DISTINCT lon FROM SOLARDETAILS').fetchall()
+	latitudes = c.execute('select DISTINCT lat FROM SOLARDETAILS').fetchall()
 	conn.close()
 else:
 	energyplot = c.execute('select totalmwh FROM SOLARDETAILS where dataloggerid="{}"'.format(userselector)).fetchall()
 	incenergyplot = c.execute('select incrementmwh FROM SOLARDETAILS where dataloggerid="{}"'.format(userselector)).fetchall()
 	blocknumber = c.execute('select block FROM SOLARDETAILS where dataloggerid="{}"'.format(userselector)).fetchall()
 	datetime = c.execute('select period FROM SOLARDETAILS where dataloggerid="{}"'.format(userselector)).fetchall()
+	longitudes = c.execute('select DISTINCT lon FROM SOLARDETAILS where dataloggerid="{}"'.format(userselector)).fetchall()
+	latitudes = c.execute('select DISTINCT lat FROM SOLARDETAILS where dataloggerid="{}"'.format(userselector)).fetchall()
 	conn.close()
 
 energyplot = [float(f[0]) for f in energyplot]
 incenergyplot = [float(f[0]) for f in incenergyplot]
 blocknumber = [int(f[0]) for f in blocknumber]
+longitudes = [float(f[0][:-1]) for f in longitudes]
+latitudes = [float(f[0][:-1]) for f in latitudes]
 
-
-plt.subplots(figsize=(16, 12))
-plt.subplots_adjust(left=0.06, bottom=0.13, right=0.98, top=0.9, wspace=.2, hspace=.6)
 if xaxischooser == 'date':
 	dates = [a[0][a[0].rfind(';')+1:] for a in datetime]
 	date = [dt.datetime.strptime(d,'%Y-%m-%d %H:%M:%S') for d in dates]
-	plt.figure(1)
+	plt.figure(num=1, figsize=(16, 12))
+	plt.subplots_adjust(left=0.06, bottom=0.13, right=0.98, top=0.9, wspace=.2, hspace=.6)
 	plt.subplot(211)
 	plt.title('TOTAL Energy data from ElectriCChain')
 	plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
@@ -62,7 +68,7 @@ if xaxischooser == 'date':
 	plt.ylabel('Energy MWh')
 	plt.xlabel('Date & Time (UTC)')
 	plt.xticks(rotation=45)
-	plt.show()
+
 elif xaxischooser == 'blocks':
 	plt.figure(1)
 	plt.subplot(211)
@@ -77,8 +83,15 @@ elif xaxischooser == 'blocks':
 	plt.ylabel('Energy MWh')
 	plt.xlabel('SolarCoin Blocks')
 	plt.xticks(rotation=45)
-	plt.show()
 else:
 	print 'You must either choose "date" or "blocks", exit in 10 seconds'
 	time.sleep(10)
 	sys.exit()
+
+plt.figure(num=2, figsize=(10, 8))
+earth = Basemap(projection='mill')
+earth.drawcoastlines(color='0.50', linewidth=0.25)
+earth.fillcontinents(color='0.95')
+x,y = earth(longitudes, latitudes)
+earth.plot(x,y, 'b*', markersize=3)
+plt.show()
